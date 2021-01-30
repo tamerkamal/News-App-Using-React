a42576b83d200586cade1fa4cb583026c66e5d5e
Tamer Abdelsalam <infinite.group7@outlook.com>
2021-01-30 17:37:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// rafce + tab is the shortcut for App.js Startup 
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app'>
+            <h1>This is App</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/features/userSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        isSignedIn: false,
+        userData: null,
+        searchInput: "tech"
+    }
+});
