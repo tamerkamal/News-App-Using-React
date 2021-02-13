f92fc2ac4e8de249a1ed78e0a2084df3d59a847b
Tamer Abdelsalam <infinite.group7@outlook.com>
2021-02-13 22:00:38 +0200

    user State

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -23,3 +23,12 @@ const userSlice = createSlice({
         }
     }
 });
+
+export const { setSignedIn, setUserData, setInput, setBlogData } = userSlice.actions;
+
+export const selectSignedIn = (state) => state.user.isSignedIn;
+export const selectUserData = (state) => state.user.userData;
+export const selectSearchInput = (state) => state.user.searchInput;
+export const selectBlogData = (state) => state.user.blogData;
+
+export default userSlice.reducer;
